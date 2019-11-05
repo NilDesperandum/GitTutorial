ee827e7a820e89578b6f5bf78b4e77c5749d4f5c
student <student@lab-45-11.cs.put.poznan.pl>
2019-11-05 08:53:18 +0100

    Pierwsza zmiana

--- Program.java
+++ Program.java
@@ -1,7 +1,9 @@
+/* this is comment */
 public class MyClass {
   static void myMethod() {
    	System.out.println("Hello1!");
 	System.out.println("Hello2!");
 	System.out.println("Hello3!");
+	System.out.println("Zmiana 3");
   }
 }
