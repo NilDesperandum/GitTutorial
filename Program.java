dbaae8e065617a4cc67600321cf8bc2140c05532
student <student@lab-45-11.cs.put.poznan.pl>
2019-11-05 08:48:42 +0100

    Updated Program.java

--- Program.java
+++ Program.java
@@ -1,5 +1,7 @@
 public class MyClass {
   static void myMethod() {
-    System.out.println("Hello!");
+   	System.out.println("Hello1!");
+	System.out.println("Hello2!");
+	System.out.println("Hello3!");
   }
 }
